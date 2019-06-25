823775bde71d08765df625ff9dd0ab1cb5af431e
github username <saramschwarz@gmail.com>
2019-06-24 20:01:51 -0400

    first commit

---
+++ src/Components/FilmDetails/FilmDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FilmDetails extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>DETAILS</h1>
+      </div>
+    )
+  }
+}
+
+export default FilmDetails
---
+++ src/Components/FilmListing/FilmListing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FilmListing extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>FILMS</h1>
+      </div>
+    )
+  }
+}
+
+export default FilmListing
